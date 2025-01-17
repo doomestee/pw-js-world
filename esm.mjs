e32d1b088b39b2dbb326d5b8b7a8da92681cdf2e
Doomester <doomester@duck.com>
2025-01-17 14:06:19 +0000

    Fix #3 import issue

--- esm.mjs
+++ esm.mjs
@@ -1,9 +1,9 @@
-let WorldPlayerHelper = (await import("./dist/Helper.js")).default;
+let PWGameWorldHelper = (await import("./dist/Helper.js")).default;
 let Block = (await import("./dist/Block.js")).default;
 let BufferReader = (await import("./dist/BufferReader.js")).default;
 let Player = (await import("./dist/Player.js")).default;
 
-if ("default" in WorldPlayerHelper) WorldPlayerHelper = WorldPlayerHelper.default;
+if ("default" in PWGameWorldHelper) PWGameWorldHelper = PWGameWorldHelper.default;
 if ("default" in Block) Block = Block.default;
 if ("default" in BufferReader) BufferReader = BufferReader.default;
 if ("default" in Player) Player = Player.default;
@@ -11,9 +11,9 @@ if ("default" in Player) Player = Player.default;
 const Constants = (await import("./dist/Constants.js")).default;
 
 export default {
-    WorldPlayerHelper, Block, BufferReader, Player, Constants
+    PWGameWorldHelper, Block, BufferReader, Player, Constants
 };
 
 export {
-    WorldPlayerHelper, Block, BufferReader, Player, Constants
+    PWGameWorldHelper, Block, BufferReader, Player, Constants
 };
