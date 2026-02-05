f4b596454e37f7ecc75f091c7f4191c2d1ba670a
Doomester <doomester@duck.com>
2026-02-05 16:09:42 +0000

    forgot to export some new stuff

--- lib/index.ts
+++ lib/index.ts
@@ -3,12 +3,14 @@ export { default as PWGameWorldHelper } from "./Helper.js";
 
 export { default as Block } from "./Block.js";
 
+export { default as Label, type ILabel, TextAlignment } from "./Label.js";
+
 export { default as Player, PlayerEffect, PlayerCounters, type IPlayer, type IPlayerEffect, type IPlayerRights, type IPlayerWorldState } from "./Player.js";
 
-export { createBlockPacket, createBlockPackets } from "./util/Misc.js";
+export { createBlockPacket, createBlockPackets, listedFieldTypeToGameType } from "./util/Misc.js";
 export * as Util from "./util/Misc.js";
 
-export { default as StructureHelper, DeserialisedStructure, type IStructure, type IStructureBlocks } from "./Structure.js";
+export { default as StructureHelper, DeserialisedStructure, type IStructure, type IStructureBlocks, IStructureBlocksV1, IStructureBlocksV2, IStructureV1, IStructureV2 } from "./Structure.js";
 
 export { LayerType } from "./Constants.js";
 
